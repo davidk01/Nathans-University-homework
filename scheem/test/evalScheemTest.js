6d0ad3377660787c23b7ca017a7e0a03d489d660
david karapetyan <dkarapetyan@gmail.com>
2012-05-01 20:20:19 -0700

    added a whole bunch of files for lessons for week 3

---
+++ scheem/test/evalScheemTest.js
@@ -0,0 +1,31 @@
+if (typeof module !== 'undefined') {
+    // In Node load required modules
+    var assert = require('chai').assert;
+    var evalScheem = require('../scheem.js').evalScheem;
+    var parse = require('../parser.js').scheem.parse;
+} else {
+    // In browser assume loaded by <script>
+    var parse = scheem.parse;
+    var assert = chai.assert;
+}
+
+suite('quote', function() {
+    test('a number', function() {
+        assert.deepEqual(
+            evalScheem(['quote', 3], {}),
+            3
+        );
+    });
+    test('an atom', function() {
+        assert.deepEqual(
+            evalScheem(['quote', 'dog'], {}),
+            'dog'
+        );
+    });
+    test('a list', function() {
+        assert.deepEqual(
+            evalScheem(['quote', [1, 2, 3]], {}),
+            [1, 2, 3]
+        );
+    });
+});
